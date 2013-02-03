b69a6f627302bf334633db70fa5da00f25e8f859
Kenneth Watson <git@shrimpworks.za.net>
2013-02-03 18:43:48 +0000

    Moved character faction class into character.php This is mostly to test if stuff still works...

--- includes/eve/api/character.php
+++ includes/eve/api/character.php
@@ -43,11 +43,9 @@
             
             $this->characterID = $character->characterID;
             $this->name = $character->name;
-
-            //$this->load($account, $characterID);
         }
 
-        function load() {//)$account, $characterID) {
+        function load() {
             $data = new apiRequest('char/CharacterSheet.xml.aspx', array($this->account->userId,
                                                                          $this->account->apiKey, 
                                                                          $this->characterID));
@@ -259,4 +257,33 @@
                 }
             }
         }
-    }?>
+    }
+    
+    class eveCharacterFaction {
+        var $factionID = 0;
+        var $factionName = '';
+        var $enlisted = 0;
+        var $currentRank = 0;
+        var $highestRank = 0;
+        var $killsYesterday = 0;
+        var $killsLastWeek = 0;
+        var $killsTotal = 0;
+        var $victoryPointsYesterday = 0;
+        var $victoryPointsLastWeek = 0;
+        var $victoryPointsTotal = 0;
+
+        function eveCharacterFaction($acc, $db, $faction) {
+            $this->factionID = (int)$faction->trainingTypeID;
+            $this->factionName = (string)$faction->factionName;
+            $this->enlisted = strtotime((string)$faction->enlisted) + $acc->timeOffset;
+            $this->currentRank = (int)$faction->currentRank;
+            $this->highestRank = (int)$faction->highestRank;
+            $this->killsYesterday = (int)$faction->killsYesterday;
+            $this->killsLastWeek = (int)$faction->killsLastWeek;
+            $this->killsTotal = (int)$faction->killsTotal;
+            $this->victoryPointsYesterday = (int)$faction->victoryPointsYesterday;
+            $this->victoryPointsLastWeek = (int)$faction->victoryPointsLastWeek;
+            $this->victoryPointsTotal = (int)$faction->victoryPointsTotal;
+        }
+    }
+?>
