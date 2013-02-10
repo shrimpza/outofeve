6d0f88609ee80b399db3bdb18aa23e8e46db9a19
Kenneth Watson <git@shrimpworks.za.net>
2013-02-10 15:13:00 +0000

    Update issue 36 Market orders updated with new keys and API. Also, updates issue 43 regarding sorting. It looks as though all market activity now passes through this API, may need to filter the results to show ORDERS only, not all buys and sells. TODO: Once corporation data loading is back, need to use wallet divisions.

--- plugins/orders/plugin.php
+++ plugins/orders/plugin.php
@@ -18,36 +18,45 @@ class orders extends Plugin {
     }
 
     function getContent() {
-        if (!isset($_GET['accountKey']))
+        if (!isset($_GET['accountKey'])) {
             $_GET['accountKey'] = 1000;
-        if (!isset($_GET['complete']))
-            $_GET['complete'] = 0;
-
-        if (isset($_GET['corp'])) {
-            $_GET['accountKey'] = max($_GET['accountKey'], 0);
-            $this->site->character->corporation->loadOrders($_GET['accountKey']);
-            $orderList = $this->site->character->corporation->orders;
-        } else {
-            $ml = new eveMarketOrderList();
-            $ml->load($this->site->eveAccount, $this->site->character);
-            $orderList = $ml->orders;
         }
+        if (!isset($_GET['complete'])) {
+            $_GET['complete'] = 0;
+        }
+        
+        if (isset($_GET['corp'])) {
+            if (eveKeyManager::getKey($this->site->user->corp_apikey_id) != null) {
+                $_GET['accountKey'] = max($_GET['accountKey'], 1000);
+                $o = new eveMarketOrderList(eveKeyManager::getKey($this->site->user->corp_apikey_id), $_GET['accountKey']);
+                $o->load();
+            }
+        } else {
+            if (eveKeyManager::getKey($this->site->user->char_apikey_id) != null) {
+                $o = new eveMarketOrderList(eveKeyManager::getKey($this->site->user->char_apikey_id));
+                $o->load();
+            }
+        }
+
+        $orderList = $o->orders;
 
         $buying = array();
         $selling = array();
 
         for ($i = 0; $i < count($orderList); $i++) {
             if (($_GET['complete'] > 0) || (($_GET['complete'] == 0) && ($orderList[$i]->orderState == 0) && ($orderList[$i]->remainingTime > 0))) {
-                if ($orderList[$i]->buying)
+                if ($orderList[$i]->buying) {
                     $buying[] = objectToArray($orderList[$i], array('DBManager', 'eveDB'));
-                else
+                } else {
                     $selling[] = objectToArray($orderList[$i], array('DBManager', 'eveDB'));
+                }
             }
         }
 
         $vars = array('buying' => $buying, 'selling' => $selling, 'corp' => isset($_GET['corp']), 'accountKey' => $_GET['accountKey'], 'complete' => $_GET['complete']);
 
         if (isset($_GET['corp'])) {
+            // todo: come back to this
             $vars['accounts'] = objectToArray($this->site->character->corporation->walletDivisions, array('DBManager', 'eveDB'));
         }
 
