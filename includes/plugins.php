79bdd13e242cc976498140472284a03fe79fc8be
Kenneth Watson <git@shrimpworks.za.net>
2013-03-04 19:59:34 +0000

    Update issue 51 Smarty 3 update appears functional, also seems it may be providing a small reduction in memory usage. Some functions updated, otherwise everything else works as-is. Removed Smarty 2 from project.

--- includes/plugins.php
+++ includes/plugins.php
@@ -13,26 +13,26 @@ class Plugin {
     }
 
     function render($template, $vars) {
-        $tpl = new Smarty();
+        $smarty = new Smarty();
 
-        $tpl->register_modifier('eveNum', 'eveNum');
-        $tpl->register_modifier('eveNumInt', 'eveNumInt');
-        $tpl->register_modifier('eveRoman', 'eveRoman');
-        $tpl->register_modifier('formatTime', 'formatTime');
-        $tpl->register_modifier('yesNo', 'yesNo');
+        $smarty->registerPlugin('modifier', 'eveNum', 'eveNum');
+        $smarty->registerPlugin('modifier', 'eveNumInt', 'eveNumInt');
+        $smarty->registerPlugin('modifier', 'eveRoman', 'eveRoman');
+        $smarty->registerPlugin('modifier', 'formatTime', 'formatTime');
+        $smarty->registerPlugin('modifier', 'yesNo', 'yesNo');
 
-        $tpl->template_dir = $GLOBALS['config']['plugins']['directory'] . '/' . get_class($this) . '/templates';
-        $tpl->compile_dir = $GLOBALS['config']['templates']['compile_dir'];
+        $smarty->setTemplateDir($GLOBALS['config']['plugins']['directory'] . '/' . get_class($this) . '/templates');
+        $smarty->setCompileDir($GLOBALS['config']['templates']['compile_dir']);
 
         foreach ($vars as $var => $value) {
-            $tpl->assign($var, $value);
+            $smarty->assign($var, $value);
         }
 
-        $tpl->assign('site_url', $GLOBALS['config']['site']['url']);
-        $tpl->assign('url_params', $_GET);
-        $tpl->assign('theme', $GLOBALS['config']['templates']['theme']);
+        $smarty->assign('site_url', $GLOBALS['config']['site']['url']);
+        $smarty->assign('url_params', $_GET);
+        $smarty->assign('theme', $GLOBALS['config']['templates']['theme']);
 
-        return $tpl->fetch($template . '.html');
+        return $smarty->fetch($template . '.html');
     }
 
 //      function getContent() {
