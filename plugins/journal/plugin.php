114234d257a358b8bdb72b555f9ae85af162d0a7
Kenneth Watson <git@shrimpworks.za.net>
2013-08-14 19:12:50 +0000

    Update issue 58 Begin with tax implementation.

--- plugins/journal/plugin.php
+++ plugins/journal/plugin.php
@@ -5,6 +5,12 @@ class journal extends Plugin {
     var $name = 'Journal';
     var $level = 1;
 
+    /*
+     * Taxable refTypeIDs - to be used when a member list is not available. 
+     * Likely less accurate but will work for API keys which don't have access to corp member lists.
+     */
+    var $taxableRefs = [33, 34, 85];
+
     function journal($db, $site) {
         $this->Plugin($db, $site);
 
@@ -48,128 +54,138 @@ class journal extends Plugin {
         }
 
         if (isset($_GET['type']) && ($_GET['type'] == 'days')) {
-            $days = array();
+            return $this->getJorunalByDay($journal, $dayCount);
+        } else if (isset($_GET['type']) && ($_GET['type'] == 'tax')) {
+            // TODO show only tax items
+        } else {
+            return $this->getJournalAsList($journal);
+        }
+    }
 
-            $refs = array();
+    function getJorunalByDay($journal, $dayCount) {
+        $days = array();
 
-            if (!isset($_GET['filter'])) {
-                $filter = -1;
-            } else {
-                $filter = $_GET['filter'];
+        $refs = array();
+
+        if (!isset($_GET['filter'])) {
+            $filter = -1;
+        } else {
+            $filter = $_GET['filter'];
+        }
+
+        for ($i = 0; $i < count($journal); $i++) {
+            if (!isset($refs[$journal[$i]['refTypeID']])) {
+                $refs[$journal[$i]['refTypeID']] = $journal[$i]['refType'];
             }
 
-            for ($i = 0; $i < count($journal); $i++) {
-                if (!isset($refs[$journal[$i]['refTypeID']])) {
-                    $refs[$journal[$i]['refTypeID']] = $journal[$i]['refType'];
+            if (($filter <= 0) || ($journal[$i]['refTypeID'] == $filter)) {
+                $jDate = date('Y-m-d', $journal[$i]['date']);
+                if (!isset($days[$jDate])) {
+                    $days[$jDate] = array(
+                        'date' => $journal[$i]['date'],
+                        'dr' => 0,
+                        'cr' => 0,
+                        'journal' => array(),
+                    );
                 }
 
-                if (($filter <= 0) || ($journal[$i]['refTypeID'] == $filter)) {
-                    $jDate = date('Y-m-d', $journal[$i]['date']);
-                    if (!isset($days[$jDate])) {
-                        $days[$jDate] = array(
-                            'date' => $journal[$i]['date'],
+                if ($filter == -1) {
+                    if (!isset($days[$jDate]['journal'][$journal[$i]['refTypeID']])) {
+                        $days[$jDate]['journal'][$journal[$i]['refTypeID']] = array(
+                            'refType' => $journal[$i]['refType'],
+                            'amount' => 0,
                             'dr' => 0,
                             'cr' => 0,
-                            'journal' => array(),
                         );
                     }
-
-                    if ($filter == -1) {
-                        if (!isset($days[$jDate]['journal'][$journal[$i]['refTypeID']])) {
-                            $days[$jDate]['journal'][$journal[$i]['refTypeID']] = array(
-                                'refType' => $journal[$i]['refType'],
-                                'amount' => 0,
-                                'dr' => 0,
-                                'cr' => 0,
-                            );
-                        }
-                        $days[$jDate]['journal'][$journal[$i]['refTypeID']]['amount'] += $journal[$i]['amount'];
-
-                        if ($journal[$i]['amount'] < 0) {
-                            $days[$jDate]['journal'][$journal[$i]['refTypeID']]['dr'] += $journal[$i]['amount'];
-                        } else {
-                            $days[$jDate]['journal'][$journal[$i]['refTypeID']]['cr'] += $journal[$i]['amount'];
-                        }
-                    } else {
-                        $days[$jDate]['journal'][] = $journal[$i];
-                    }
+                    $days[$jDate]['journal'][$journal[$i]['refTypeID']]['amount'] += $journal[$i]['amount'];
 
                     if ($journal[$i]['amount'] < 0) {
-                        $days[$jDate]['dr'] += $journal[$i]['amount'];
+                        $days[$jDate]['journal'][$journal[$i]['refTypeID']]['dr'] += $journal[$i]['amount'];
                     } else {
-                        $days[$jDate]['cr'] += $journal[$i]['amount'];
+                        $days[$jDate]['journal'][$journal[$i]['refTypeID']]['cr'] += $journal[$i]['amount'];
                     }
+                } else {
+                    $days[$jDate]['journal'][] = $journal[$i];
+                }
+
+                if ($journal[$i]['amount'] < 0) {
+                    $days[$jDate]['dr'] += $journal[$i]['amount'];
+                } else {
+                    $days[$jDate]['cr'] += $journal[$i]['amount'];
                 }
             }
-
-            asort($refs);
-
-            if (count($days) > $dayCount) {
-                $days = array_chunk($days, $dayCount);
-
-                $pageCount = count($days);
-                $pageNum = max((int) $_GET['p'], 0);
-                $nextPage = min($pageNum + 1, $pageCount);
-                $prevPage = max($pageNum - 1, 0);
-
-                $days = $days[$pageNum];
-            } else {
-                $pageCount = 0;
-                $pageNum = 0;
-                $nextPage = 0;
-                $prevPage = 0;
-            }
-
-            $vars = array('days' => $days, 'dayCount' => $dayCount, 'filter' => $filter, 'refTypes' => $refs,
-                'pageCount' => $pageCount, 'pageNum' => $pageNum, 'nextPage' => $nextPage, 'prevPage' => $prevPage,
-                'corp' => isset($_GET['corp']), 'accountKey' => $_GET['accountKey']);
-
-            if (isset($_GET['corp'])) {
-                if (eveKeyManager::getKey($this->site->user->corp_apikey_id) != null) {
-                    $corpKey = eveKeyManager::getKey($this->site->user->corp_apikey_id);
-                    $corporation = new eveCorporation($corpKey);
-                    $corporation->load();
-                }
-                $vars['accounts'] = objectToArray($corporation->walletDivisions);
-            }
-
-            return $this->render('days', $vars);
-        } else {
-            if (count($journal) > 50) {
-                $journal = array_chunk($journal, 50);
-
-                $pageCount = count($journal);
-                $pageNum = max((int) $_GET['p'], 0);
-                $nextPage = min($pageNum + 1, $pageCount);
-                $prevPage = max($pageNum - 1, 0);
-
-                $journal = $journal[$pageNum];
-            } else {
-                $pageCount = 0;
-                $pageNum = 0;
-                $nextPage = 0;
-                $prevPage = 0;
-            }
-
-            foreach ($journal as $k => $j) {
-                $journal[$k]['reason'] = $this->getJornalReason($j);
-            }
-
-            $vars = array('journal' => $journal, 'pageCount' => $pageCount,
-                'pageNum' => $pageNum, 'nextPage' => $nextPage, 'prevPage' => $prevPage,
-                'corp' => isset($_GET['corp']), 'accountKey' => $_GET['accountKey']);
-
-            if (isset($_GET['corp'])) {
-                if (eveKeyManager::getKey($this->site->user->corp_apikey_id) != null) {
-                    $corpKey = eveKeyManager::getKey($this->site->user->corp_apikey_id);
-                    $corporation = new eveCorporation($corpKey);
-                    $corporation->load();
-                }
-                $vars['accounts'] = objectToArray($corporation->walletDivisions);
-            }
-
-            return $this->render('journal', $vars);
         }
+
+        asort($refs);
+
+        if (count($days) > $dayCount) {
+            $days = array_chunk($days, $dayCount);
+
+            $pageCount = count($days);
+            $pageNum = max((int) $_GET['p'], 0);
+            $nextPage = min($pageNum + 1, $pageCount);
+            $prevPage = max($pageNum - 1, 0);
+
+            $days = $days[$pageNum];
+        } else {
+            $pageCount = 0;
+            $pageNum = 0;
+            $nextPage = 0;
+            $prevPage = 0;
+        }
+
+        $vars = array('days' => $days, 'dayCount' => $dayCount, 'filter' => $filter, 'refTypes' => $refs,
+            'pageCount' => $pageCount, 'pageNum' => $pageNum, 'nextPage' => $nextPage, 'prevPage' => $prevPage,
+            'corp' => isset($_GET['corp']), 'accountKey' => $_GET['accountKey']);
+
+        if (isset($_GET['corp'])) {
+            if (eveKeyManager::getKey($this->site->user->corp_apikey_id) != null) {
+                $corpKey = eveKeyManager::getKey($this->site->user->corp_apikey_id);
+                $corporation = new eveCorporation($corpKey);
+                $corporation->load();
+            }
+            $vars['accounts'] = objectToArray($corporation->walletDivisions);
+        }
+
+        return $this->render('days', $vars);
+    }
+
+    function getJournalAsList($journal) {
+        if (count($journal) > 50) {
+            $journal = array_chunk($journal, 50);
+
+            $pageCount = count($journal);
+            $pageNum = max((int) $_GET['p'], 0);
+            $nextPage = min($pageNum + 1, $pageCount);
+            $prevPage = max($pageNum - 1, 0);
+
+            $journal = $journal[$pageNum];
+        } else {
+            $pageCount = 0;
+            $pageNum = 0;
+            $nextPage = 0;
+            $prevPage = 0;
+        }
+
+        foreach ($journal as $k => $j) {
+            $journal[$k]['reason'] = $this->getJornalReason($j);
+        }
+
+        $vars = array('journal' => $journal, 'pageCount' => $pageCount,
+            'pageNum' => $pageNum, 'nextPage' => $nextPage, 'prevPage' => $prevPage,
+            'corp' => isset($_GET['corp']), 'accountKey' => $_GET['accountKey']);
+
+        if (isset($_GET['corp'])) {
+            if (eveKeyManager::getKey($this->site->user->corp_apikey_id) != null) {
+                $corpKey = eveKeyManager::getKey($this->site->user->corp_apikey_id);
+                $corporation = new eveCorporation($corpKey);
+                $corporation->load();
+            }
+            $vars['accounts'] = objectToArray($corporation->walletDivisions);
+        }
+
+        return $this->render('journal', $vars);
     }
 
     function getJornalReason($j) {
